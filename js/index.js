1f4c711ecd5ce41ff121be0a6a672c1755988c23
Cintia <cintia.dev2024@gmail.com>
2025-02-14 00:20:13 -0300

    modificacoes_14_02_25

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let index = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll(".slide");
+    const totalSlides = slides.length;
+
+    index += direction;
+
+    if (index < 0) {
+        index = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        index = 0;
+    }
+
+    document.querySelector(".carousel").style.transform = `translateX(${-index * 100}%)`;
+}
+
+function toggleMenu() {
+    document.querySelector('.navbar').classList.toggle('active');
+    document.querySelector('.menu-toggle').classList.toggle('active');
+}
